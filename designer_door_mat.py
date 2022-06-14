d5c96f74c71da1edf6c5735cfbf909aa021edf9b
Diego Alejandro Lozano Millan <diegoalejandrolozano@hotmail.es>
2022-06-14 15:57:58 -0500

    v1. Se crea el código para generar el patrón

---
+++ designer_door_mat.py
@@ -0,0 +1,29 @@
+'''
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+'''
+
+def main():
+    n, m = map(int, input('Ingrese N y M: ').split(' '))
+    rango_medio = n//2
+    contador = 0
+    #rango superior
+    for i in range(0, rango_medio):
+        patron =  '.|.'*i + '.|.' + '.|.'*i
+        print(patron.center(m, '-'))
+    #imprimiento la mitad
+    print('WELCOME'.center(m, '-'))
+    #rango superior
+    for i in range((rango_medio-1), -1, -1):
+        patron =  '.|.'*i + '.|.' + '.|.'*i
+        print(patron.center(m, '-'))
+
+if __name__ == '__main__':
+    main()
